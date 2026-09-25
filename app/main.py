078beee53c18b34a926771ddd121fa8e9a0a3919
Asad Ullah Shamim <www.asadullahshamim@gmail.com>
2026-09-25 21:20:01 +0600

    allow_origins updated

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from app.submissions.router import router as submission_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="CodeAssess API", description="Developer Assessment & Coding Platform API", version="1.0.0")
-app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:5173", "http://127.0.0.1:5173",], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://codeassessaus.netlify.app"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
